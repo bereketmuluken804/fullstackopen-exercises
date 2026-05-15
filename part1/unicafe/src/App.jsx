7b442cb9ff936ebd8f5390a69781beae121baad1
M. Bereket <bmuluken804@gmail.com>
2026-05-15 15:58:02 +0300

    Complete exercises 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,27 +1,35 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const App = () => {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+  const total =  good + neutral + bad;
+  const rawAvg = (good - bad) / total;
+  const avg = Math.round(rawAvg * 100) / 100;
+  const pos = Math.round((good/total) * 100) /100
+	return (
+		<>
+			<div>
+				<h1>How is our service?</h1>
+				<button onClick={() => setGood(good + 1)}>😁Good</button>
+				<button onClick={() => setNeutral(neutral + 1)}>
+					😏Neutral
+				</button>
+				<button onClick={() => setBad(bad + 1)}>😒Bad </button>
+			</div>
+			<div id="stat">
+        
+				<h2>Statistics</h2>
+				<p>Good: {good} </p>
+				<p>Neutral: {neutral} </p>
+				<p>Bad: {bad} </p>
+				<p>Total feedback: {total}</p>
+        <p>Average ( -1 to 1): {avg || 0} </p>
+				<p>positve: {pos || 0}%</p>
+			</div>
+		</>
+	);
+};
 
-    return (
-      <>
-      <div>
-        <h1>How is our service?</h1>
-        <button onClick={ ()=> setGood(good + 1)}>😁Good</button>
-        <button onClick={ ()=> setNeutral(neutral + 1)}>😏Neutral</button>
-        <button onClick={ ()=> setBad(bad + 1)}>😒Bad </button>
-      </div>
-      <div id="stat">
-        <h2>Statistics</h2>
-        <p>Good: {good} </p>
-        <p>Neutral: {neutral} </p>
-        <p>Bad: { bad } </p>
-      </div>
-      </>
-    )
-}
-
-
-export default App
+export default App;
