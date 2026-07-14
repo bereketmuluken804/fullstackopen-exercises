bcfb01171965fd81aecd6285f3d1958de09cda02
M. Bereket <bmuluken804@gmail.com>
2026-07-14 13:52:29 +0300

    exercises: 4.3

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,20 +1,26 @@
 import express from "express";
 import mongoose from "mongoose";
 import blogRouter from "./controller/blogs.js";
-import { unknownEndpont, requestLogger,errorLogger } from "./utils/middleware.js";  
+import {
+	unknownEndpont,
+	requestLogger,
+	errorLogger,
+} from "./utils/middleware.js";
 import config from "./utils/config.js";
 import logger from "./utils/logger.js";
 import Blog from "./model/blog.js";
 
-
 const app = express();
-logger.info("connecting to database")
+logger.info("connecting to database");
 
-mongoose.connect(config.DB_URL, {family: 4})
-  .then(()=> logger.info("connected to MongoDb"))
-  .catch((err)=>logger.error("Couldn't connect to the database: \n", err.message));
+mongoose
+	.connect(config.DB_URL, { family: 4 })
+	.then(() => logger.info("connected to MongoDb"))
+	.catch((err) =>
+		logger.error("Couldn't connect to the database: \n", err.message),
+	);
 
-app.use(express.json())
+app.use(express.json());
 app.use(requestLogger);
 app.use("/api/blogs", blogRouter);
 
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import config from './utils/config.js'
+import express from "express";
+import config from "./utils/config.js";
 import app from "./app.js";
-const PORT = config.PORT
+const PORT = config.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+	console.log(`Server running on port ${PORT}`);
+});
